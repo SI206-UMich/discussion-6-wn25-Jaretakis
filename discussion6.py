947d1e299dd85be6902d8e41d88187b473b88404
James Aretakis <jamesaretakis@Jamess-MacBook-Pro-33.local>
2025-02-20 11:18:38 -0500

    Finished

--- discussion6.py
+++ discussion6.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import csv
 
 
 def load_csv(f):
@@ -18,6 +19,15 @@ def load_csv(f):
     base_path = os.path.abspath(os.path.dirname(__file__))
     full_path = os.path.join(base_path, f)
     # use this 'full_path' variable as the file that you open
+    with open(full_path) as fh:
+        r = csv.reader(fh)
+        rows = []
+        print(f"Add the data from the csv")
+        for row in r:
+            print(f"Adding {row} to rows")
+            rows.append(row)
+        print(f"Final Value of rows is {rows}")
+
 
 def get_annual_max(d):
     '''
